f82d41fd52f82a7a904d4780a946758e5e7b5255
Михаил Скляров <sklyarov.m@roistat.com>
2020-06-08 02:09:16 +0300

    Foooo...

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -14,9 +14,6 @@ git pull origin master
 {{-- Install missing PHP dependencies --}}
 php72 ~/bin/composer install --no-interaction --no-dev --prefer-dist
 
-npm install
-npm run prod
-
 {{-- Maintenance mode OFF --}}
 php72 artisan up
 
