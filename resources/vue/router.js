90e50a141ae6eeeb01143f501308aefb89da8486
Михаил Скляров <sklyarov.m@roistat.com>
2020-06-10 00:44:14 +0300

    Metronic installation

---
+++ resources/vue/router.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      redirect: "/dashboard",
+      component: () => import("@/view/layout/Layout"),
+      children: [
+        {
+          path: "/dashboard",
+          name: "dashboard",
+          component: () => import("@/view/pages/Dashboard.vue")
+        },
+      ],
+    },
+  ]
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,18 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
+// mix.js('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css')
+//     .version()
+//     .sourceMaps();
+
+mix.js('resources/vue/main.js', 'public/js')
     .version()
-    .sourceMaps();
+    .sourceMaps()
+    .webpackConfig({
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'resources/vue')
+            }
+        }
+    });
