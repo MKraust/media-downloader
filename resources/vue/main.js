5919e7a1ed01aecb62336b9b4c9042a5f336b2d1
Михаил Скляров <sklyarov.m@roistat.com>
2020-06-20 16:21:17 +0300

    Production assets

--- resources/vue/main.js
+++ resources/vue/main.js
@@ -14,12 +14,9 @@ import "popper.js";
 import "tooltip.js";
 import PerfectScrollbar from "perfect-scrollbar";
 window.PerfectScrollbar = PerfectScrollbar;
-// import ClipboardJS from "clipboard";
-// window.ClipboardJS = ClipboardJS;
 
 // Vue 3rd party plugins
 import lodash from 'lodash';
-import "./core/plugins/portal-vue";
 import "./core/plugins/bootstrap-vue";
 import "./core/plugins/perfect-scrollbar";
 import "./core/plugins/inline-svg";
