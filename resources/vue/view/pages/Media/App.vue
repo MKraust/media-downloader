a87b4c36be4918f3c5c7b29e13fbb22e26916921
Михаил Скляров <sklyarov.m@roistat.com>
2020-06-21 20:27:06 +0300

    Test deploy

--- resources/vue/view/pages/Media/App.vue
+++ resources/vue/view/pages/Media/App.vue
@@ -3,7 +3,7 @@
 
     <div v-if="isLoading || media === null" class="d-flex justify-content-center" style="height: 400px;">
       <div v-if="isLoading" class="spinner spinner-track spinner-primary spinner-lg" style="margin-left: -1rem;"></div>
-      <p v-else-if="media === null" class="text-center">Медиа контент не найден</p>
+      <p v-else-if="media === null" class="text-center">Медиа контент не найден.</p>
     </div>
 
     <div v-else>
