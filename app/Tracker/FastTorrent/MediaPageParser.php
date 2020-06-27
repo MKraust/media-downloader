61b7170cacc21d4fd689cb1eacc482a10a3e55c7
Михаил Скляров <sklyarov.m@roistat.com>
2020-06-28 02:51:11 +0300

    [Fast Torrent] Parse original title

--- app/Tracker/FastTorrent/MediaPageParser.php
+++ app/Tracker/FastTorrent/MediaPageParser.php
@@ -13,6 +13,11 @@ class MediaPageParser
         $media['poster'] = $infoNode->filter('.info-panel > .film-image > a')->first()->attr('href');
         $media['title'] = $page->filter('#film_name')->first()->text();
 
+        $originalTitleNodes = $page->filter('#film_name_orig');
+        if (count($originalTitleNodes) > 0) {
+            $media['original_title'] = $originalTitleNodes->first()->text();
+        }
+
         return $media;
     }
 }
--- app/Tracker/FastTorrent/SearchResultsParser.php
+++ app/Tracker/FastTorrent/SearchResultsParser.php
@@ -8,24 +8,32 @@ use Symfony\Component\DomCrawler\Crawler;
 class SearchResultsParser extends BaseSearchResultsParser
 {
     protected function getMediaItemsNodes(Crawler $document) {
-        return $document->filter('.film-list .film-item .film-image');
+        return $document->filter('.film-list .film-item');
     }
 
     protected function getTitle(Crawler $mediaNode): string {
-        return $mediaNode->attr('alt');
+        return $mediaNode->filter('.film-image')->first()->attr('alt');
     }
 
     protected function getOriginalTitle(Crawler $mediaNode): ?string {
-        return null;
+        $headingNodes = $mediaNode->filter('.film-wrap > h2 > span');
+        $originalTitle = null;
+        $headingNodes->each(function (Crawler $headingNode) use (&$originalTitle) {
+            if ($headingNode->attr('itemprop') === 'alternativeHeadline') {
+                $originalTitle = $headingNode->text();
+            }
+        });
+
+        return $originalTitle;
     }
 
     protected function getLink(Crawler $mediaNode): string {
-        $relativeUrl = $mediaNode->children()->eq(0)->attr('href');
+        $relativeUrl = $mediaNode->filter('.film-image')->first()->children()->eq(0)->attr('href');
         return Requester::BASE_URL . $relativeUrl;
     }
 
     protected function getPoster(Crawler $mediaNode): string {
-        $linkStyle = $mediaNode->children()->eq(0)->attr('style');
+        $linkStyle = $mediaNode->filter('.film-image')->first()->children()->eq(0)->attr('style');
         return str_replace(['background: url(', ')', '/cache', '_video_list'], '', $linkStyle);
     }
 }
