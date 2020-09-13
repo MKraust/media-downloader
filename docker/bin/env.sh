15e2c8ad710caaa4a6c9fa51d30bd85e207a9be8
Михаил Скляров <sklyarov.m@roistat.com>
2020-09-14 00:25:55 +0300

    Move docker files into own directory

---
+++ docker/bin/env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $(dirname ${SCRIPT_DIR}/../..)
+
+export $(cat ${SCRIPT_DIR}/../.env | xargs)
+
+PROJECT_NAME=torrents
+DOCKER_FILE="-f docker-compose.yml"
+
+COMPOSE="docker-compose -p ${PROJECT_NAME} ${DOCKER_FILE}"
