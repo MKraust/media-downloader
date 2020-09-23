65b3b9fbde331f86f104b5d6181974b6578b5a80
Mikhail <kraust137@gmail.com>
2020-09-23 18:43:12 +0300

    Wtf?!

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new Jobs\RefreshTorrentDownloads)->everyMinute();
-        $schedule->job(new Jobs\CheckFavoritesForNewEpisodes)->everyTwoHours();
+        $schedule->job(new Jobs\CheckFavoritesForNewEpisodes)->everyThreeHours();
     }
 
     /**
