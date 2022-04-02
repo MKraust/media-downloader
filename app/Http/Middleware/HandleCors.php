a09930a040d3f6745c657053c5ff63334795cb25
Sklyarov Mikhail <msklyarov@ozon.ru>
2022-04-02 18:23:28 +0300

    Use own CORS middleware

---
+++ app/Http/Middleware/HandleCors.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HandleCors
+{
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', '*');
+    }
+}
