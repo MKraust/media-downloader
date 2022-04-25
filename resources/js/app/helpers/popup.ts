70fa2d23833d0217e6517304cd0750c1b926644c
MKraust <kraust137@gmail.com>
2022-04-25 22:02:11 +0300

    Enable confirmation popups

--- resources/js/app/helpers/popup.ts
+++ resources/js/app/helpers/popup.ts
@@ -1,15 +1,14 @@
-// import Swal from 'sweetalert2'
+import Swal from 'sweetalert2'
 
 export const confirm = async (title: string, text: string) => {
-  return true
-  // const { value } = await Swal.fire({
-  //   title,
-  //   text,
-  //   icon: 'question',
-  //   showCancelButton: true,
-  //   confirmButtonText: 'Да',
-  //   cancelButtonText: 'Отмена',
-  // })
-  //
-  // return value
+  const { value } = await Swal.fire({
+    title,
+    text,
+    icon: 'question',
+    showCancelButton: true,
+    confirmButtonText: 'Да',
+    cancelButtonText: 'Отмена',
+  })
+
+  return value
 }
